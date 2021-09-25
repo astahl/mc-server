263165b80ea866315190c1b78e28383876aeee56
Andreas Stahl <andreasbjornstahl@gmail.com>
2021-09-25 00:47:02 +0000

    added post handling and initial server jar start

--- start.py
+++ start.py
@@ -1,7 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import os
+from subprocess import Popen
 from pathlib import Path
+import subprocess
+import uuid
 
 
 hostName = "0.0.0.0"
@@ -10,19 +13,15 @@ worldDirectory = Path("worlds")
 jarsDirectory = Path("jars")
 
 
-def writeJsonResponse(self, obj, response=200):
-    self.send_response(response)
-    self.send_header("Content-type", "text/json")
-    self.end_headers()
-    serialized = json.dumps(obj)
-    self.wfile.write(bytes(serialized, "utf-8"))
+def createWorld(version):
+    jarFile = jarsDirectory.joinpath(version).joinpath("server.jar")
+    id = uuid.uuid4()
+    worldPath = worldDirectory.joinpath(id.hex)
+    worldPath.mkdir(parents=True)
+    print("creating world %s" % id)
+    serverProcess = subprocess.run(["java", "-Xmx6G", "-Xms6G", "-jar", jarFile.resolve(), "nogui"], cwd=worldPath, capture_output=True)
+    print(serverProcess.stdout)
 
-def writeTxtResponse(self, message, response=200):
-    self.send_response(response)
-    self.send_header("Content-type", "text/txt")
-    self.end_headers()
-    self.wfile.write(bytes(message, "utf-8"))
-        
 
 def versions():
     return [x.name for x in jarsDirectory.iterdir() if x.is_dir()]
@@ -31,23 +30,48 @@ def versions():
 class MCControlServer(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/versions":
-            writeJsonResponse(self, versions())
+            self.writeJsonResponse(versions())
         else:
-            writeTxtResponse(self, "404 - File not found", 404)
-        
+            self.writeTxtResponse("404 - File not found", 404)
+    
+    def do_POST(self):
+        if self.path == "/worlds":
+            # curl -X POST -H "Content-Type: application/json" -d '{"version": "1.17.1"}' "localhost:8080/worlds"
+            data = self.readJsonRequest()
+            if data is None:
+                return
+            createWorld(data["version"])
+
+    def writeJsonResponse(self, obj, response=200):
+        serialized = json.dumps(obj)
+        self.send_response(response)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Content-length", str(len(serialized)))
+        self.end_headers()
+        self.wfile.write(serialized.encode("utf-8"))
+
+    def writeTxtResponse(self, message: str, response=200):
+        self.send_response(response)
+        self.send_header("Content-type", "text/txt")
+        self.send_header("Content-length", str(len(message)))
+        self.end_headers()
+        self.wfile.write(message.encode("utf-8"))
+
+    def readJsonRequest(self):
+        content_length = self.headers.get("Content-length")
+        if content_length is None:
+            self.send_error(411)
+            return None
+        length = int(content_length)
+        return json.loads(self.rfile.read(length))
+
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MCControlServer)
+    print("Server starting at http://%s:%s" % (hostName, serverPort))
+    with HTTPServer((hostName, serverPort), MCControlServer) as webServer:
+        try: 
+            webServer.serve_forever()
+        except KeyboardInterrupt:
+            pass
 
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try: 
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
     print("Server stopped")
-
-
-
